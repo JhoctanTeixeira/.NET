cd0c64348e9b6caa18a4fec678bd310176f410cb
Jhoctan Teixeira <80040630+JhoctanTeixeira@users.noreply.github.com>
2022-08-03 22:47:45 -0300

    Add files via upload

---
+++ Fiap.Aula01/Fiap.Aula01.UI/Models/Time.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Aula01.UI.Models
+{
+    public class Time
+    {
+        //Atributos (Campos/Fields)
+        private string _formacao;
+        private int _titulos;
+
+        //Propiedade 
+        public string Nome { get; set; }
+        public bool Mundial { get; set; }
+
+
+        //Propiedade para o titulo
+        public int Titulos
+        {
+            get { return _titulos; }
+            set { _titulos = value; }
+        }
+
+        //Encapsulamento Mesmo resultado que criar os atributos
+   
+        public string Formacao
+        {
+            get { return _formacao; }
+            set { _formacao = value; }
+        }
+    }
+}
---
+++ Fiap.Aula01/Fiap.Aula01.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using Fiap.Aula01.UI.Models;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Instanciar um Time 
+Time palmeiras = new Time();
+
+//Atribuir uma formação para o time
+palmeiras.Formacao = "4-2-3-1";
+//Exibir a formação do time
+Console.WriteLine(palmeiras.Formacao);
